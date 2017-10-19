741155f8b6ad577da3e9bd449720ab8b19ee5094
GabrielGrimberg <gabby.gr97@hotmail.com>
2017-10-20 00:02:14 +0100

    Changes to Splash Screen

---
+++ VitonBet/app/src/main/java/com/vitonbet/vitonbet/SplashActivity.java
@@ -0,0 +1,26 @@
+/**
+ * Name: VitonBet
+ * Type: Group Assignment
+ * Class: SplashActivity
+ * Revision: 1.0
+ * Notes: TBA
+ */
+
+package com.vitonbet.vitonbet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
