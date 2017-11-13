6c7e6b2878344ad36cdd2d859d028887381e93bd
GabrielGrimberg <gabby.gr97@hotmail.com>
2017-11-13 23:04:45 +0000

    Register Functionality Complete

--- Application/app/src/main/java/vitonbet/com/vitonbet/MainActivity.java
+++ Application/app/src/main/java/vitonbet/com/vitonbet/MainActivity.java
@@ -1,11 +1,19 @@
 package vitonbet.com.vitonbet;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
@@ -14,11 +22,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText passField;
     private TextView signInLink;
 
+    //private ProgressDialog progressDialog;
+
+    //Firebase Auth Object
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //progressDialog = new ProgressDialog(this);
 
         btnSubmit = (Button) findViewById(R.id.btnRegister);
 
@@ -29,6 +46,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnSubmit.setOnClickListener(this);
         signInLink.setOnClickListener(this);
+    }
+
+    private void UserRegister()
+    {
+        String email = emailField.getText().toString().trim();
+        String password = passField.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email))
+        {
+            //Email is empty.
+            Toast.makeText(this, "Enter in a valid email.", Toast.LENGTH_SHORT).show();
+
+            //Stop exe further
+            return;
+        }
+
+        if(TextUtils.isEmpty(password))
+        {
+            //Password is empty.
+            Toast.makeText(this, "Enter in a valid password.", Toast.LENGTH_SHORT).show();
+
+            //Stop exe further
+            return;
+        }
+
+        //TODO
+        //If all is okay, continue on to main screen. (Change activity.)
+        //progressDialog.setMessage("Registering, Please Wait...");
+        //progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if(task.isSuccessful())
+                        {
+                            //Successfully registered and logged in.
+                            //Go to main screen activity.
+                            //For now display message
+                            Toast.makeText(MainActivity.this, "Registered Successfuly.", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "Failed to Register.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
     }
 
     @Override
