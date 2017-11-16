2817c73e7195d661d003bdf9b59a467c7baf6f51
GabrielGrimberg <gabby.gr97@hotmail.com>
2017-11-16 23:28:05 +0000

    Register UI Improvements + New Fields

--- VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/RegisterActivity.java
+++ VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/RegisterActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -23,6 +22,9 @@ public class RegisterActivity extends AppCompatActivity
     private EditText xNameField;
     private EditText xEmailField;
     private EditText xPasswordField;
+    private EditText xRePasswordField;
+    private EditText xDOBField;
+    private EditText xPhoneField;
 
     private Button xRegisterBtn;
 
@@ -48,6 +50,10 @@ public class RegisterActivity extends AppCompatActivity
         xNameField = (EditText) findViewById(R.id.nameField);
         xEmailField = (EditText) findViewById(R.id.emailField);
         xPasswordField = (EditText) findViewById(R.id.passwordField);
+        xRePasswordField = (EditText) findViewById(R.id.repasswordField);
+        xDOBField = (EditText) findViewById(R.id.dobField);
+        xPhoneField = (EditText) findViewById(R.id.phoneField);
+
         xRegisterBtn = (Button) findViewById(R.id.registerBtn);
 
         xRegisterBtn.setOnClickListener(new View.OnClickListener()
@@ -56,7 +62,6 @@ public class RegisterActivity extends AppCompatActivity
             public void onClick(View view)
             {
                 startRegister();
-
             }
         });
     }
@@ -64,11 +69,21 @@ public class RegisterActivity extends AppCompatActivity
     private void startRegister()
     {
         final String name = xNameField.getText().toString().trim();
-        String email = xEmailField.getText().toString().trim();
+        final String dob = xDOBField.getText().toString().trim();
+        final String phone = xPhoneField.getText().toString().trim();
+
+        final String email = xEmailField.getText().toString().trim();
         String password = xPasswordField.getText().toString().trim();
+        String repassword = xRePasswordField.getText().toString().trim();
 
         //Check if user is submitting a field empty.
-        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password))
+        if(!TextUtils.isEmpty(name) &&
+                !TextUtils.isEmpty(email) &&
+                !TextUtils.isEmpty(password) &&
+                !TextUtils.isEmpty(repassword) &&
+                !TextUtils.isEmpty(dob) &&
+                !TextUtils.isEmpty(phone))
+
         {
             xProgress.setMessage("Signing Up...");
             xProgress.show();
@@ -83,7 +98,10 @@ public class RegisterActivity extends AppCompatActivity
 
                        DatabaseReference current_user_db = xDatabase.child(user_id);
 
-                       current_user_db.child("name").setValue(name);
+                       current_user_db.child("username").setValue(name);
+                       current_user_db.child("dob").setValue(dob);
+                       current_user_db.child("phone").setValue(phone);
+                       current_user_db.child("email").setValue(email);
 
                        xProgress.dismiss();
 
