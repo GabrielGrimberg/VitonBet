5d7db3f09ecd014871348cd74aff1c3ff9ad23f4
GabrielGrimberg <gabby.gr97@hotmail.com>
2017-11-18 23:53:10 +0000

    Casino - Roulette Started

---
+++ VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/CasinoActivity.java
@@ -0,0 +1,15 @@
+package gabrielgrimberg.com.vitonbet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CasinoActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_casino);
+    }
+}
