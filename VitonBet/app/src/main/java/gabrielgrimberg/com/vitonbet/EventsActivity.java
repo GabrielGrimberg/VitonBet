1ce571ca0b346f6245ddfa432f936a206a01a4ee
Majiick <ecostequse@gmail.com>
2017-11-24 19:59:17 +0000

    xD

--- VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/EventsActivity.java
+++ VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/EventsActivity.java
@@ -1,5 +1,11 @@
 package gabrielgrimberg.com.vitonbet;
 
+
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,22 +13,40 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-public class EventsActivity extends AppCompatActivity
+public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFragmentInteractionListener
 {
     String[] teams = {"Na'Vi", "Virtus Dno", "Chelsea FC", "ManUnited"};
     ArrayList<String> listItems=new ArrayList<String>();
     ArrayAdapter<String> adapter;
+
+    public void onFragmentInteraction(Uri u) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+        findViewById(R.id.enterEvent).setVisibility(View.GONE);
 
         adapter=new CusAdapter(this,
                 android.R.layout.simple_list_item_1,
@@ -39,8 +63,59 @@ public class EventsActivity extends AppCompatActivity
 
         Random random = new Random();
         for(int i = 0; i < 10; i++) {
-            addItems(teams[random.nextInt(4)] + " VS. " + teams[random.nextInt(4)]);
+            //addItems(teams[random.nextInt(4)] + " VS. " + teams[random.nextInt(4)]);
         }
+
+        Button addNewBtn = findViewById(R.id.addNew);
+        addNewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.enterEvent).setVisibility(View.VISIBLE);
+                findViewById(R.id.list).setVisibility(View.GONE);
+                findViewById(R.id.addNew).setVisibility(View.GONE);
+            }
+        });
+
+        Button submit = findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //FirebaseAuth xAuth = FirebaseAuth.getInstance();
+                DatabaseReference xDatabase = FirebaseDatabase.getInstance().getReference().child("Events");
+
+                EditText title = findViewById(R.id.title);
+                EditText description = findViewById(R.id.description);
+                EditText note = findViewById(R.id.note);
+                xDatabase.child(title.getText().toString()).child("description").setValue(description.getText().toString());
+                xDatabase.child(title.getText().toString()).child("note").setValue(note.getText().toString());
+
+                findViewById(R.id.enterEvent).setVisibility(View.GONE);
+                findViewById(R.id.list).setVisibility(View.VISIBLE);
+                findViewById(R.id.addNew).setVisibility(View.VISIBLE);
+
+                Toast.makeText(EventsActivity.this, "Your submission will be reviwed by an admin.",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference()
+                .child("Events");
+        db.addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                for (DataSnapshot event : dataSnapshot.getChildren()) {
+                    addItems(event.getKey());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+                // ...
+            }
+        });
     }
 
     public void addItems(String s) {
