2e4f2b05697232e0314bd90e65b6135f26bae579
GabrielGrimberg <gabby.gr97@hotmail.com>
2017-11-24 16:22:50 +0000

    Main Page - UI Improvements

--- VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/MainActivity.java
+++ VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/MainActivity.java
@@ -104,7 +104,6 @@ public class MainActivity extends AppCompatActivity
                         logout();
 
                         return true;
-
                 }
                 return true;
             }
