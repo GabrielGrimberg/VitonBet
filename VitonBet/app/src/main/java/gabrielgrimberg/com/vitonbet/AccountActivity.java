d715324b0d3258c61e75a35789d506a7b6a6e3ac
GabrielGrimberg <gabby.gr97@hotmail.com>
2017-11-25 22:48:15 +0000

    Navigator Update

--- VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/AccountActivity.java
+++ VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/AccountActivity.java
@@ -1,11 +1,23 @@
 package gabrielgrimberg.com.vitonbet;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AccountActivity extends AppCompatActivity
 {
+    //Top nav vars.
+    private DrawerLayout xDrawerLayout;
+    private ActionBarDrawerToggle xToggle;
+    private FirebaseAuth xAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -16,5 +28,80 @@ public class AccountActivity extends AppCompatActivity
         Helper.SetUsername(this);
         Helper.SetDOB(this);
         Helper.SetEmail(this);
+
+        xAuth = FirebaseAuth.getInstance();
+
+        /* Top nav. */
+        xDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        xToggle = new ActionBarDrawerToggle(this, xDrawerLayout, R.string.open, R.string.close);
+
+        xDrawerLayout.addDrawerListener(xToggle);
+        xToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView xNavigationView = (NavigationView) findViewById(R.id.top_nav_id);
+
+        /* Activity Change when Item from Top Navigator is Clicked */
+        xNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem)
+            {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.nav_account:
+                        Intent accountActivity = new Intent(getApplicationContext(), AccountActivity.class);
+                        accountActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(accountActivity);
+
+                        return true;
+
+                    case R.id.nav_home:
+                        Intent homeActivity = new Intent(getApplicationContext(), MainActivity.class);
+                        homeActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(homeActivity);
+
+                        return true;
+
+                    case R.id.nav_events:
+                        Intent eventsActivity = new Intent(getApplicationContext(), EventsActivity.class);
+                        eventsActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(eventsActivity);
+
+                        return true;
+
+                    case R.id.nav_casino:
+                        Intent casinoActivity = new Intent(getApplicationContext(), CasinoActivity.class);
+                        casinoActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(casinoActivity);
+
+                        return true;
+
+                    case R.id.nav_ad:
+                        Intent adActivity = new Intent(getApplicationContext(), AdActivity.class);
+                        adActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(adActivity);
+
+                        return true;
+
+                    case R.id.nav_logout:
+                        xAuth.signOut();
+
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(xToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/EventsActivity.java
+++ VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/EventsActivity.java
@@ -27,19 +27,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
-class BetEvent {
+class BetEvent
+{
     public String title;
     public String description;
 }
 
-public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFragmentInteractionListener, ViewEvent.OnFragmentInteractionListener
-{
+public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFragmentInteractionListener, ViewEvent.OnFragmentInteractionListener {
     String[] teams = {"Na'Vi", "Virtus Dno", "Chelsea FC", "ManUnited"};
-    ArrayList<String> listItems=new ArrayList<String>();
+    ArrayList<String> listItems = new ArrayList<String>();
     ArrayAdapter<String> adapter;
     static List<BetEvent> events = new ArrayList<>();
 
-    public void onFragmentInteraction(Uri u) {
+    public void onFragmentInteraction(Uri u)
+    {
 
     }
 
@@ -48,29 +49,36 @@ public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFr
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+
         findViewById(R.id.enterEvent).setVisibility(View.GONE);
         findViewById(R.id.viewEvent).setVisibility(View.GONE);
 
         Helper.SetBalance(this);
 
-        adapter=new CusAdapter(this,
+        adapter = new CusAdapter(this,
                 android.R.layout.simple_list_item_1,
                 listItems);
         final ListView lv = findViewById(R.id.list);
+
         lv.setAdapter(adapter);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
                 findViewById(R.id.viewEvent).setVisibility(View.VISIBLE);
                 findViewById(R.id.list).setVisibility(View.GONE);
                 findViewById(R.id.addNew).setVisibility(View.GONE);
 
 
-                String s = (String)adapterView.getItemAtPosition(i);
+                String s = (String) adapterView.getItemAtPosition(i);
                 Log.d("mytag", s);
 
-                for(BetEvent be : events) { //Why not use a hashmap? xDDD cuz lazy
-                    if (be.title == s) {
+                //Use HashMap. This will do for now.
+                for (BetEvent be : events)
+                {
+                    if (be.title == s)
+                    {
                         EditText t = findViewById(R.id.betTitle2);
                         t.setText(be.title);
                         EditText d = findViewById(R.id.description);
@@ -82,19 +90,17 @@ public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFr
             }
         });
 
-        /*
-
-         */
-
-
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             //addItems(teams[random.nextInt(4)] + " VS. " + teams[random.nextInt(4)]);
         }
 
         Button addNewBtn = findViewById(R.id.addNew);
-        addNewBtn.setOnClickListener(new View.OnClickListener() {
+        addNewBtn.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 findViewById(R.id.enterEvent).setVisibility(View.VISIBLE);
                 findViewById(R.id.list).setVisibility(View.GONE);
                 findViewById(R.id.addNew).setVisibility(View.GONE);
@@ -102,9 +108,11 @@ public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFr
         });
 
         Button submit = findViewById(R.id.submit);
-        submit.setOnClickListener(new View.OnClickListener() {
+        submit.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 //FirebaseAuth xAuth = FirebaseAuth.getInstance();
                 DatabaseReference xDatabase = FirebaseDatabase.getInstance().getReference().child("Events");
 
@@ -124,17 +132,20 @@ public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFr
         });
 
         Button submitBet = findViewById(R.id.placeBet);
-        submitBet.setOnClickListener(new View.OnClickListener() {
+        submitBet.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 findViewById(R.id.viewEvent).setVisibility(View.GONE);
                 findViewById(R.id.list).setVisibility(View.VISIBLE);
                 findViewById(R.id.addNew).setVisibility(View.VISIBLE);
 
 
-                String betAmount = ((EditText)findViewById(R.id.betAmount)).getText().toString();
-                String title = ((EditText)findViewById(R.id.betTitle2)).getText().toString();
-                if (Integer.parseInt(betAmount) > Integer.parseInt(((TextView)findViewById(R.id.balance)).getText().toString().replace("€", "").replace("Balance: ", ""))) {
+                String betAmount = ((EditText) findViewById(R.id.betAmount)).getText().toString();
+                String title = ((EditText) findViewById(R.id.betTitle2)).getText().toString();
+
+                if (Integer.parseInt(betAmount) > Integer.parseInt(((TextView) findViewById(R.id.balance)).getText().toString().replace("€", "").replace("Balance: ", ""))) {
                     Toast.makeText(EventsActivity.this, "You don't have enough balance.",
                             Toast.LENGTH_LONG).show();
 
@@ -144,24 +155,30 @@ public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFr
                 Toast.makeText(EventsActivity.this, "You will get notified when the result of the event is out.",
                         Toast.LENGTH_LONG).show();
 
-                class BetResult extends AsyncTask<String, Void, Void> {
+                class BetResult extends AsyncTask<String, Void, Void>
+                {
                     AppCompatActivity a;
                     boolean succ = false;
                     String eventTitle;
                     int betAmount;
 
-                    public BetResult(AppCompatActivity a) {
+                    public BetResult(AppCompatActivity a)
+                    {
                         this.a = a;
                     }
 
-                    protected Void doInBackground(String... event) {
-                        try {
+                    protected Void doInBackground(String... event)
+                    {
+                        try
+                        {
                             eventTitle = event[0];
                             betAmount = Integer.parseInt(event[1]);
                             Thread.sleep(20000);
 
-                        } catch (InterruptedException e) {
-                            // xD
+                        }
+                        catch (InterruptedException e)
+                        {
+                            // Some error.
                         }
 
                         return null;
@@ -170,12 +187,13 @@ public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFr
                     protected void onPostExecute(Void unused)
                     {
                         Random r = new Random();
-                        if(r.nextBoolean())
+
+                        if (r.nextBoolean())
                         {
-                            Toast.makeText(a, "You won your bet on " + eventTitle + " and won " + Integer.toString(betAmount*2) + " euro.",
+                            Toast.makeText(a, "You won your bet on " + eventTitle + " and won " + Integer.toString(betAmount * 2) + " euro.",
                                     Toast.LENGTH_LONG).show();
 
-                            Helper.AddBalance(a, betAmount*2);
+                            Helper.AddBalance(a, betAmount * 2);
                         }
                         else
                         {
@@ -194,16 +212,18 @@ public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFr
 
         DatabaseReference db = FirebaseDatabase.getInstance().getReference()
                 .child("Events");
+
         db.addListenerForSingleValueEvent(new ValueEventListener()
         {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot)
             {
-                for (DataSnapshot event : dataSnapshot.getChildren()) {
+                for (DataSnapshot event : dataSnapshot.getChildren())
+                {
                     addItems(event.getKey());
                     BetEvent be = new BetEvent();
                     be.title = event.getKey();
-                    be.description = ((HashMap<String, String>)event.getValue()).get("description");
+                    be.description = ((HashMap<String, String>) event.getValue()).get("description");
                     events.add(be);
                 }
             }
@@ -216,22 +236,27 @@ public class EventsActivity extends AppCompatActivity implements EnterEvent.OnFr
         });
     }
 
-    public void addItems(String s) {
+    public void addItems(String s)
+    {
         listItems.add(s);
         adapter.notifyDataSetChanged();
     }
 
-    class CusAdapter extends ArrayAdapter<String> implements View.OnClickListener {
-        CusAdapter(Context c, int x, ArrayList<String> s) {
+    class CusAdapter extends ArrayAdapter<String> implements View.OnClickListener
+    {
+        CusAdapter(Context c, int x, ArrayList<String> s)
+        {
             super(c, x, s);
         }
 
         @Override
-        public void onClick(View v) {
-            int position=(Integer) v.getTag();
-            Object object= getItem(position);
-            String teams = (String)object;
+        public void onClick(View v)
+        {
+            int position = (Integer) v.getTag();
+            Object object = getItem(position);
+            String teams = (String) object;
             Log.d("mytag", teams);
         }
     }
+
 }
