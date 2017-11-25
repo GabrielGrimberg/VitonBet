0532203607e9c91122b925172e18371c1591e9b4
GabrielGrimberg <gabby.gr97@hotmail.com>
2017-11-25 22:34:12 +0000

    Casino Page - Navigator

--- VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/CasinoActivity.java
+++ VitonBet/app/src/main/java/gabrielgrimberg/com/vitonbet/CasinoActivity.java
@@ -11,8 +11,13 @@ Last updated: 22nd of November.
  */
 package gabrielgrimberg.com.vitonbet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.DecelerateInterpolator;
@@ -24,6 +29,8 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Random;
 
@@ -46,11 +53,81 @@ public class CasinoActivity extends AppCompatActivity
     int oldDegree = 0;
     public int betAmount = 0;
 
+    //Top nav vars.
+    private DrawerLayout xDrawerLayout;
+    private ActionBarDrawerToggle xToggle;
+    private FirebaseAuth xAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_casino);
+
+        xAuth = FirebaseAuth.getInstance();
+
+        /* Top nav. */
+        xDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        xToggle = new ActionBarDrawerToggle(this, xDrawerLayout, R.string.open, R.string.close);
+
+        xDrawerLayout.addDrawerListener(xToggle);
+        xToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView xNavigationView = (NavigationView) findViewById(R.id.top_nav_id);
+
+        /* Activity Change when Item from Top Navigator is Clicked */
+        xNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem)
+            {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.nav_account:
+                        Intent accountActivity = new Intent(getApplicationContext(), AccountActivity.class);
+                        accountActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(accountActivity);
+
+                        return true;
+
+                    case R.id.nav_home:
+                        Intent homeActivity = new Intent(getApplicationContext(), MainActivity.class);
+                        homeActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(homeActivity);
+
+                        return true;
+
+                    case R.id.nav_events:
+                        Intent eventsActivity = new Intent(getApplicationContext(), EventsActivity.class);
+                        eventsActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(eventsActivity);
+
+                        return true;
+
+                    case R.id.nav_casino:
+                        Intent casinoActivity = new Intent(getApplicationContext(), CasinoActivity.class);
+                        casinoActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(casinoActivity);
+
+                        return true;
+
+                    case R.id.nav_ad:
+                        Intent adActivity = new Intent(getApplicationContext(), AdActivity.class);
+                        adActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(adActivity);
+
+                        return true;
+
+                    case R.id.nav_logout:
+                        xAuth.signOut();
+
+                        return true;
+                }
+                return true;
+            }
+        });
 
 
         Helper.SetBalance(this);
@@ -237,5 +314,15 @@ public class CasinoActivity extends AppCompatActivity
 
 
         green.setSelected(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(xToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
